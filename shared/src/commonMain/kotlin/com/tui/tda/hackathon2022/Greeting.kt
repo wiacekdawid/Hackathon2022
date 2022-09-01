9521f7694480f2976bb5084881dbb49d0c8a246c
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-09-01 14:47:51 +0100

    Init project

---
+++ shared/src/commonMain/kotlin/com/tui/tda/hackathon2022/Greeting.kt
@@ -0,0 +1,7 @@
+package com.tui.tda.hackathon2022
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/tui/tda/hackathon2022/Platform.kt
@@ -0,0 +1,5 @@
+package com.tui.tda.hackathon2022
+
+expect class Platform() {
+    val platform: String
+}
